c05233d1279128a42994acc5ba80e137268d8c46
Abivarshi <abivarshi.15@cse.mrt.ac.lk>
2017-07-11 02:59:30 +0530

    Calendar styles included

--- Calendar.php
+++ Calendar.php
@@ -6,28 +6,14 @@
     <title> Calendar </title>
     <link rel="stylesheet" type = "text/css" href = "Styles/stylesheets.css"/>
     <style>
-        input[type = text]{
-            width: 100%;
-            height: 30px;
-            display: inline-block;
-            border-radius: 4px;
-            box-sizing: border-box;
-        }
 
         input[type=submit]{
-
             padding: 14px 20px;
-            width: 100%;
-            background-color: #4CAF50;
+            width: 20%;
+            margin: 0px 10px 0px 180px;
+            background-color: #490c01;
             border-radius: 2px;
-        }
-
-        input[type = date]{
-            width: 100%;
-            height: 30px;
-            display: inline-block;
-            border-radius: 4px;
-            box-sizing: border-box;
+            font-style: inherit;
         }
 
         form[name = fixedform]{
@@ -41,8 +27,8 @@
             background-color: darkgrey;
         }
 
-        div[id = message]{
-            color: crimson;
+        div[id = message_2]{
+            color: #7a0a0c;
             margin-top: 10px;
             padding: 14px 20px;
             width: auto ;
@@ -51,6 +37,14 @@
 
         div[id = message_1]{
             color: #000000;
+            margin-top: 10px;
+            padding: 14px 20px;
+            width: auto ;
+            border-radius: 2px;
+        }
+
+        div[id = message]{
+            color: crimson;
             margin-top: 10px;
             padding: 14px 20px;
             width: auto ;
@@ -87,100 +81,52 @@
 
     <div id="content_area">
         <h2 class="heading">School Calendar</h2>
-        <form action="Calendar.php" method="get" name="fixedform">
-            <fieldset>
-                <label>Year:</label><br><br>
-                <input type="text" name="year" placeholder="Year"><br><br><br>
-                <input type="submit" value="submit">
-                <?php
-                require_once "./Connect/Connect.php";
-                $message = '';
-                if ($link || mysqli_select_db($link,$database)){
-                    //echo "Connection successful;";
-                } else {
-                    $message="connection failed";
-                }
-                if (isset($_GET['year'])){
-                    if(!empty($_GET['year'])) {
-                        if($_GET['year'] !== (string)(int) $_GET['year']) {
-                            $message='Year can only be integer number'.'<br>';
-                        }else if((int)$_GET['year']<1990 OR (int)$_GET['year']>date("Y")){
-                            $message='Year should be in range 1991-'.date('Y').'<br>';
-                        }else{
-                            $year=(int)$_GET['year'];
-                            $sql = "SELECT * FROM calendar WHERE YEAR(Date)=$year ORDER BY Date";
-                            $result = mysqli_query($link, $sql);
-                            if ($result) {
-                                if (mysqli_num_rows($result)>0) {
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        echo '<div id="message_1">';
-                                        echo $row['Date']." : ".$row['Event'];
-                                        echo '</div>';
-                                    }
-                                }else{
-                                    $message="No events!!!";
-                                }
+        <form action="CalendarYr.php" name="fixedform">
+        <?php
+        require_once "./Connect/Connect.php";
+        $message = '';
+        if ($link || mysqli_select_db($link,$database)){
+            //echo "Connection successful;";
+        } else {
+            $message="connection failed";
+        }
+        if (isset($_GET['year'])){
+            if(!empty($_GET['year'])) {
+                if($_GET['year'] !== (string)(int) $_GET['year']) {
+                    $message='Year can only be integer number'.'<br>';
+                }else if((int)$_GET['year']<1990 OR (int)$_GET['year']>date("Y")){
+                    $message='Year should be in range 1991-'.date('Y').'<br>';
+                }else{
+                    $year=(int)$_GET['year'];
+                    $sql = "SELECT * FROM calendar WHERE YEAR(Date)=$year ORDER BY Date";
+                    $result = mysqli_query($link, $sql);
+                    if ($result) {
+                        if (mysqli_num_rows($result)>0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo '<div id="message_1">';
+                                echo $row['Date']." : ".'<div id="message_2">'.$row['Event'].'</div>'.'</div>';
                             }
+                        }else{
+                            $message="No events!!!";
                         }
-                    }else{$message='Year field can not take an empty value';}
-                }else {$message='Year field should be filled';}
-                ?>
-                <div id="message">
-                    <?php
-                    echo $message
-                    ?>
-                </div>
-            </fieldset>
+                    }
+                }
+            }else{$message='Year field can not take an empty value';}
+        }else {$message='Year field should be filled';}
+        ?>
+        <div id="message">
+            <?php
+            echo $message.'<br>';
+            ?>
+            <input type="submit" value="OK" style="color: #ffffff">
+        </div>
         </form>
     </div>
 
-    <div id="sidebar">
-        <nav id="competition">
-            <ul id="nav">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-bottom: 0px"> <a href="compDetail.php">Competition Details</a></li>
-            </ul>
-        </nav>
-
-        <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-            <ul id="nav" style="margin-top: 0px">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 20px"> <a href="Calendar.php">School Calendar</a></li>
-            </ul>
-        </nav>
-
-        <?php
-        session_start();
-        $username = $_SESSION['username'];
-
-        if ($username == 'principal'){
-            ?>
-
-            <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-                <ul id="nav" style="margin-top: 0px">
-                    <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 45px"> <a href="../addStaff.php">Add Staff</a></li>
-                </ul>
-            </nav>
-
-            <?php
-        }
-        ?>
-
-
-    </div>
-
-    <footer>
-        <div class = 'footer1'>
-            <h3 id="h3">Address</h3>
-            J/St.John Bosco Vidyalayam,<br/>
-            Racca Road, Jaffna.
-        </div>
-        <div class = 'footer2'>
-            <h3 id="h3" >Contact Us</h3>
-            Email : stjohnbosco@yahoo.com<br />
-            Tel: Principal office: +940212222540
-        </div>
-        <div class = 'footer3'><i>copyright : Futura Labs</i></div>
-
-    </footer>
+    <?php
+    include 'Styles/SidebarStyle.html';
+    include 'Styles/FooterStyle.html';
+    ?>
 
 </div>
 </body>
--- CalendarYr.php
+++ CalendarYr.php
@@ -5,6 +5,9 @@
 
     <title>Calendar</title>
     <link rel="stylesheet" type = "text/css" href = "Styles/stylesheets.css"/>
+    <?php
+    include 'Styles/FormStyle.html';
+    ?>
 
 </head>
 <body>
@@ -22,63 +25,21 @@
     </nav>
 
     <div id="content_area">
-        <form action="Calendar.php" method="get">
+        <h2 class="heading">Enter Year to view the School Calendar</h2>
+        <form action="Calendar.php" method="get" name="fixedform">
             <fieldset>
-                <label>Enter Year:</label><br><br>
+                <label>Year:</label><br><br>
                 <input type="text" name="year" placeholder="Year"><br><br><br>
-                <button type="submit">Submit</button>
+                <input type="submit" value="submit">
             </fieldset>
         </form>
 
     </div>
 
-    <div id="sidebar">
-        <nav id="competition">
-            <ul id="nav">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-bottom: 0px"> <a href="../compDetail.php">Competition Details</a></li>
-            </ul>
-        </nav>
-
-        <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-            <ul id="nav" style="margin-top: 0px">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 20px"> <a href="../Calendar.php">School Calendar</a></li>
-            </ul>
-        </nav>
-
-        <?php
-        session_start();
-        $username = $_SESSION['username'];
-
-        if ($username == 'principal'){
-            ?>
-
-            <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-                <ul id="nav" style="margin-top: 0px">
-                    <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 45px"> <a href="../addStaff.php">Add Staff</a></li>
-                </ul>
-            </nav>
-
-            <?php
-        }
-        ?>
-
-
-    </div>
-
-    <footer>
-        <div class = 'footer1'>
-            <h3 id="h3">Address</h3>
-            J/St.John Bosco Vidyalayam,<br/>
-            Racca Road, Jaffna.
-        </div>
-        <div class = 'footer2'>
-            <h3 id="h3" >Contact Us</h3>
-            Email : stjohnbosco@yahoo.com<br />
-            Tel: Principal office: +940212222540
-        </div>
-        <div class = 'footer3'><i>copyright : Futura Labs</i></div>
-
-    </footer>
+    <?php
+    include 'Styles/SidebarStyle.html';
+    include 'Styles/FooterStyle.html';
+    ?>
 
 </div>
 </body>
