f7fa566db1ca359b2f4bfba00caa654034686097
Gunavaran <gunavaran.15@cse.mrt.ac.lk>
2017-07-10 23:45:27 +0530

    Redundant code reduced

--- Attendance/updateattendance.php
+++ Attendance/updateattendance.php
@@ -82,53 +82,10 @@
 
     </div>
 
-    <div id="sidebar">
-        <nav id="competition">
-            <ul id="nav">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-bottom: 0px"> <a href="../compDetail.php">Competition Details</a></li>
-            </ul>
-        </nav>
-
-        <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-            <ul id="nav" style="margin-top: 0px">
-                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 20px"> <a href="../Calendar.php">School Calendar</a></li>
-            </ul>
-        </nav>
-
-        <?php
-        session_start();
-        $username = $_SESSION['username'];
-
-        if ($username == 'principal'){
-            ?>
-
-            <nav id="competition" style="margin-top: 0px; padding-top: 0px">
-                <ul id="nav" style="margin-top: 0px">
-                    <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 45px"> <a href="../User/addStaff.php">Add Staff</a></li>
-                </ul>
-            </nav>
-
-            <?php
-        }
-        ?>
-
-
-    </div>
-
-    <footer>
-        <div class = 'footer1'>
-            <h3 id="h3">Address</h3>
-            J/St.John Bosco Vidyalayam,<br/>
-            Racca Road, Jaffna.
-        </div>
-        <div class = 'footer2'>
-            <h3 id="h3" >Contact Us</h3>
-            Email : stjohnbosco@yahoo.com<br />
-            Tel: Principal office: +940212222540
-        </div>
-        <div class = 'footer3'><i>copyright : Futura Labs</i></div>
-
-    </footer>
+    <?php
+    include '../Styles/SidebarStyle.html';
+    include '../Styles/FooterStyle.html';
+    ?>
 
 </div>
 
