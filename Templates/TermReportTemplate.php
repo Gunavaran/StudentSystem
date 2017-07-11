afb677799522bfc7f7eff73a8bd4763d8afad16a
vahesan1995 <vahesan.15@cse.mrt.ac.lk>
2017-07-11 06:28:07 +0530

    Term report template styles added

--- Templates/TermReportTemplate.php
+++ Templates/TermReportTemplate.php
@@ -1,24 +1,124 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Vahe
-*/
+<html>
+<head>
+    <title> Get Term Exam Report</title>
+    <link rel="stylesheet" type = "text/css" href = "../Styles/stylesheets.css"/>
+    <style>
+        input[type = text]{
+            width: 100%;
+            height: 30px;
+            display: inline-block;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
 
-$content= "<form action=\"../TermExam/termReport.php\" method=\"get\">
-                IndexNo:<br>
-                    <input type='text' name='indexno' >
-                    <br>
-                Year:<br>
-                    <input type='text' name='year' >
-                    <br>
-                Term:
-                    <select name=\"term\">
-                        <option value=\"1\">1</option>
-                        <option value=\"2\">2</option>
-                        <option value=\"3\">3</option>
-                        </select><br><br>
-                <input type=\"submit\" value=\"Submit\">
-           </form>";
+        input[type=submit]{
 
-include 'BasicTemplate.php';
-?>
+            padding: 14px 20px;
+            width: 100%;
+            background-color: #4CAF50;
+            border-radius: 2px;
+        }
+
+        form[name = fixedform]{
+            float: left;
+            width: 40%;
+            margin: 20px 10px 0px 300px;
+            padding: 10px;
+            border: 2px solid #E3E3E3;
+            border-radius: 5px;
+            font-family: "Adobe Gothic Std B";
+            background-color: darkgrey;
+        }
+
+        div[id = message]{
+            color: crimson;
+            margin-top: 10px;
+            padding: 14px 20px;
+            width: auto ;
+            border-radius: 2px;
+        }
+
+        .heading{
+            margin-left: 300px;
+        }
+        nav[id=competition]{
+            background-color: mediumorchid;
+            height:60px;
+            border-radius: 5px;
+            margin-top: 10px;
+        }
+    </style>
+</head>
+<body>
+<div id="wrapper">
+    <div id="banner"></div>
+
+    <nav id="navigation">
+        <ul id="nav">
+            <li><a href="index.php"> Home </a> </li>
+            <li> <a href="#">Profile</a></li>
+            <li> <a href="MarksTemplate.php">Marks</a></li>
+            <li> <a href="attendancetemplate.php">Attendance</a></li>
+            <li> <a href="../Log_in_out/logout.php">Logout</a></li>
+        </ul>
+    </nav>
+
+    <div id="content_area">
+        <h2>Enter the following details to obtain Report</h2>
+        <form action="../TermExam/termReport.php" method="get" name="fixedform">
+            IndexNo:<br>
+            <input type='text' name='indexno' >
+            <br>
+            Year:<br>
+            <input type='text' name='year' >
+            <br>
+            Term:
+            <select name="term">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+            </select><br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </div>
+    <div id="sidebar">
+        <nav id="competition">
+            <ul id="nav">
+                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-bottom: 0px"> <a href="CompDetailTemplate.php">Competition Details</a></li>
+            </ul>
+        </nav>
+
+        <nav id="competition" style="margin-top: 0px; padding-top: 0px">
+            <ul id="nav" style="margin-top: 0px">
+                <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 20px"> <a href="../Calendar.php">School Calendar</a></li>
+            </ul>
+        </nav>
+
+            <nav id="competition" style="margin-top: 0px; padding-top: 0px">
+                <ul id="nav" style="margin-top: 0px">
+                    <li id = 'compLine' style="font-size: 20px; margin-top: 15px; margin-left: 45px"> <a href="../User/addStaff.php">Add Staff</a></li>
+                </ul>
+            </nav>
+
+
+    </div>
+
+    <footer>
+        <div class = 'footer1'>
+            <h3 id="h3">Address</h3>
+            J/St.John Bosco Vidyalayam,<br/>
+            Racca Road, Jaffna.
+        </div>
+        <div class = 'footer2'>
+            <h3 id="h3" >Contact Us</h3>
+            Email : stjohnbosco@yahoo.com<br />
+            Tel: Principal office: +940212222540
+        </div>
+        <div class = 'footer3'><i>copyright : Futura Labs</i></div>
+
+    </footer>
+
+</div>
+</body>
+</html>
+
